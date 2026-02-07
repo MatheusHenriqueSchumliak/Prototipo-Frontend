311d94e8eaa2886afeb416cce70df6af4713823e
Matheus Henrique Schumliak <matheeus.hs@gmail.com>
2026-02-07 01:14:26 -0300

    adiciona variavel de ambiente e config vercel

--- Prototipo-Front-End/src/services/Api.ts
+++ Prototipo-Front-End/src/services/Api.ts
@@ -2,7 +2,7 @@ import Swal from "sweetalert2";
 import axios from "axios";
 // Cria a instância
 const api = axios.create({
-  baseURL: "https://localhost:7058/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: false, // pode deixar como está se não usa cookies
 });
 
